a0045679518b92da1f090f9ce7142d7b0cf23624
fezeboris <borisfeze9@gmail.com>
2024-01-17 15:43:45 +0100

    all product fetch implementation

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -18,13 +18,30 @@ async function getData(category: string) {
   const data = await client.fetch(query);
   return data;
 }
+
+async function getAllData() {
+  const query = `*[_type == 'product' ]{
+        _id,
+        "imageUrl": images[0].asset->url,
+         name,
+         price,
+        'slug': slug.current,
+        'categoryName':category->name,
+
+    } `;
+  const data = await client.fetch(query);
+  return data;
+}
 export const dynamic = "force-dynamic";
 export default async function CategoryPage({
   params,
 }: {
   params: { category: string };
 }) {
+  const cathe = params.category;
   const data: simplifiedProduct[] = await getData(params.category);
+  const allData: simplifiedProduct[] = await getAllData();
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4  sm:px-6  lg:max-w-7xl lg:px-8">
@@ -34,34 +51,63 @@ export default async function CategoryPage({
           </h2>
         </div>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-4 xl:gap-x-8">
-          {data.map((product, index) => (
-            <div key={product._id} className="group relative">
-              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageUrl}
-                  alt="Product image"
-                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
-                  width={300}
-                  height={300}
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <Link href={`/product/${product.slug}`}>
-                      {product.name}
-                    </Link>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">
-                    {product.categoryName}
-                  </p>
+          {cathe === "all"
+            ? allData.map((product, index) => (
+                <div key={product._id} className="group relative">
+                  <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                    <Image
+                      src={product.imageUrl}
+                      alt="Product image"
+                      className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                      width={300}
+                      height={300}
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <Link href={`/product/${product.slug}`}>
+                          {product.name}
+                        </Link>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.categoryName}
+                      </p>
+                    </div>
+                    <p className="text-sm font-medium text-gray-900">
+                      ${product.price}
+                    </p>
+                  </div>
                 </div>
-                <p className="text-sm font-medium text-gray-900">
-                  ${product.price}
-                </p>
-              </div>
-            </div>
-          ))}
+              ))
+            : data.map((product, index) => (
+                <div key={product._id} className="group relative">
+                  <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                    <Image
+                      src={product.imageUrl}
+                      alt="Product image"
+                      className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                      width={300}
+                      height={300}
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <Link href={`/product/${product.slug}`}>
+                          {product.name}
+                        </Link>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.categoryName}
+                      </p>
+                    </div>
+                    <p className="text-sm font-medium text-gray-900">
+                      ${product.price}
+                    </p>
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
     </div>
